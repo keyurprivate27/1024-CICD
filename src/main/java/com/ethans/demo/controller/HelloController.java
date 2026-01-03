119a5a47562970588a7047c474b300c6c6ed0272
keyurprivate27 <keyurprivate27@gmail.com>
2026-01-03 14:06:03 +0530

    added hello controller

---
+++ src/main/java/com/ethans/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ethans.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String saySomething() {
+        return "Good Afternoon....! Hello Radhe Krishna...!";
+    }
+}
